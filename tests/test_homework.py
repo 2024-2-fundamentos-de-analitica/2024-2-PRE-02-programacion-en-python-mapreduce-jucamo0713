7099ea67acdeeb28a10e5a1d5b36b831533c77c9
jucamo <jucamo.0713@gmail.com>
2024-11-06 11:28:26 -0500

    finished

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -11,26 +11,26 @@ def test_01():
     """Test Word Count"""
 
     wc.run_job(
-        "files/input",
-        "files/output",
+        os.path.join("files","input"),
+        os.path.join("files","output"),
     )
 
     #
     # Retorna error si la carpeta output/ no existe
-    if not os.path.exists("files/output/"):
+    if not os.path.exists(os.path.join("files","output")):
         raise Exception("Output directory does not exist")
 
     #
     # Retorna error si el archivo "_SUCCESS" no existe en la
     # carpeta output/
-    if not os.path.exists("files/output/_SUCCESS"):
+    if not os.path.exists(os.path.join("files","output","_SUCCESS")):
         raise Exception("Output directory is empty")
 
     #
     # Lee el contenido del archivo "part-00000" en la carpeta output/
     # Cada linea en el archivo esta conformada por una clave un valor,
     # separados por un tabulador. Asigne pareja a un diccionario
-    with open("files/output/part-00000", "r", encoding="utf-8") as f:
+    with open(os.path.join("files","output","part-00000"), "r", encoding="utf-8") as f:
         lines = f.readlines()
         result = {}
         for line in lines:
